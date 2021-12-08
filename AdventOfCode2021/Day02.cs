dc5db1646723ec4aeb203cff86f4738d5ee40e2a
Alex Uhling <alex.uhling@gmail.com>
2021-12-07 20:39:27 -0500

    Day 2 part 1 complete

--- AdventOfCode2021/Day02.cs
+++ AdventOfCode2021/Day02.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2021
 {
-	internal class Day02
+	public static class Day02
 	{
+		public static IEnumerable<Instructions> ParseInstructions(params string[] input)
+		{
+			if (input is null) return new List<Instructions>();
+
+			return input.Select(x =>
+			{
+				var parts = x.Split(' ');
+				var instruction = new Instructions { Distance = int.Parse(parts[1]) };
+				switch (parts[0].ToLowerInvariant())
+				{
+					case "forward": instruction.Direction = Direction.Forward; break;
+					case "down": instruction.Direction = Direction.Down; break;
+					case "up": instruction.Direction = Direction.Up; break;
+					default:
+						break;
+				}
+				return instruction;
+			});
+		}
+		public static Location FindLocationAfterInstructions(IEnumerable<Instructions> instructions)
+		{
+			return instructions.Aggregate(new Location(), (location, instruction) =>
+			{
+				switch (instruction.Direction)
+				{
+					case Direction.Forward: location.Horizontal += instruction.Distance; break;
+					case Direction.Up: location.Depth -= instruction.Distance; break;
+					case Direction.Down: location.Depth += instruction.Distance; break;
+				}
+
+				return location;
+			});
+		}
+	}
+
+	public class Location
+	{
+		public int Horizontal { get; set; }
+		public int Depth { get; set; }
+	}
+
+	public class Instructions
+	{
+		public Direction Direction { get; set; }
+		public int Distance { get; set; }
+	}
+
+	public enum Direction
+	{
+		Forward,
+		Down,
+		Up
 	}
 }
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -4,8 +4,14 @@ using AdventOfCode2021;
 Console.WriteLine("Hello, World!");
 
 
-// Day 01 - part 1
-var day01LinesOfInput = File.ReadAllLines("Day01Input.txt");
+// Day 01
+var day01LinesOfInput = File.ReadAllLines("Inputs/Day01Input.txt");
 var day01Input = day01LinesOfInput.Select(int.Parse).ToArray();
 Console.WriteLine($"Day 01 - Part 1 - {Day01.FindNumberOfIncreasesInDepth(day01Input)}");
 Console.WriteLine($"Day 01 - Part 2 - {Day01.FindDepthIncreaseCountForWindowsOfThree(day01Input)}");
+
+// Day 02
+var day02LinesOfInput = File.ReadAllLines("Inputs/Day02Input.txt");
+var instructions = Day02.ParseInstructions(day02LinesOfInput);
+var location = Day02.FindLocationAfterInstructions(instructions);
+Console.WriteLine($"Final location {location.Horizontal}, {location.Depth}. Product = {location.Depth * location.Horizontal}");
