97f20aa0ee2b22fe073ad1b68b7052efb8fb5699
Alex Uhling <alex.uhling@gmail.com>
2022-01-04 18:53:46 -0500

    Clean up usings on day 5

--- AdventOfCode2021/Day05.cs
+++ AdventOfCode2021/Day05.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2021
+﻿namespace AdventOfCode2021
 {
 	public static class Day05
 	{
